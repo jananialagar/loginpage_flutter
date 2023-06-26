e3bca912a875491452856253db926d14f4b7e255
jananialagar <jananilagar@gmail.com>
2023-06-26 22:09:58 +0530

    commit_1

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      appBar: AppBar(
+        title: const Text(
+          ' Estee Lauder',
+          style: TextStyle(color: Colors.black),
+          textAlign: TextAlign.left,
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Container(
+            height: 400,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/fashion.jpeg'),
+                  fit: BoxFit.fitHeight),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 400,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(colors: [
+                      Colors.grey,
+                      Colors.white,
+                    ]),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromRGBO(143, 148, 251, 2),
+                          blurRadius: 10.0,
+                          offset: Offset(0, 10))
+                    ],
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        width: 400,
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.grey,
+                              Colors.white,
+                            ],
+                          ),
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email or phone number ",
+                            hintStyle: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 400,
+                        padding: const EdgeInsets.all(8.0),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password ",
+                            hintStyle: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(colors: [
+                      Colors.grey,
+                      Colors.white,
+                    ]),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color.fromRGBO(143, 148, 251, 2),
+                          blurRadius: 10.0,
+                          offset: Offset(0, 10))
+                    ],
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Forgot Password",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
